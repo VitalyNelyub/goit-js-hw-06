74f2474f9739e2374181eb0fea5350dd9c687df5
Vitaly <nelyub86@ukr.net>
2023-01-12 22:49:48 +0200

    test #8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const formEl = document.querySelector(".login-form");
 
+formEl.addEventListener("submit", submitForm);
+
+const dataFormAfterSubmit = {};
+
+function submitForm(event) {
+  event.preventDefault();
+  const EMAIL = event.currentTarget.elements.email.value;
+  const PASSWORD = event.currentTarget.elements.password.value;
+
+  if (EMAIL === "" || PASSWORD === "") {
+    alert("Заповніть всі поля форми");
+  } else {
+    dataFormAfterSubmit.email = EMAIL;
+    dataFormAfterSubmit.password = PASSWORD;
+  }
+  console.log("Данні користувача з форми: ", dataFormAfterSubmit);
+  formEl.reset();
+}
