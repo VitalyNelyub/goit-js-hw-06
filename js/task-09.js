b23f5d04b28973c82c9d45902c79a158e9b6ad5f
Vitaly <nelyub86@ukr.net>
2023-01-12 23:43:06 +0200

    test #9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
+const changeBodyColorBtn = document.querySelector(".change-color");
+const currentColor = document.querySelector(".color");
+
+changeBodyColorBtn.addEventListener("click", changeColor);
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+let foo = getRandomHexColor;
+
+function changeColor() {
+  currentColor.textContent = foo();
+  document.body.style.backgroundColor = foo();
+}
+
+
+
