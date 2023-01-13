50e36f46b751fdc147f527277e98706658f775fd
Vitaly <nelyub86@ukr.net>
2023-01-13 13:28:23 +0200

    finish with out test#10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,28 @@
+const placeForCreateEl = document.getElementById("boxes");
+const inputEl = document.querySelector("number");
+const createElem = document.querySelector("button[data-create]");
+const removeElem = document.querySelector("button[data-destroy]");
+
+console.log(createElem);
+console.log(removeElem);
+
+
+
+inputEl.addEventListener("change", currentNumber);
+let counterCreateElement = 0;
+
+
+function currentNumber(event) {
+  counterCreateElement = event.value
+  console.log(counterCreateElement);
+}
+createElem.addEventListener("click");
+removeElem.addEventListener("click");
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+function createBoxes(amount) {}
+
+function destroyBoxes() {}
